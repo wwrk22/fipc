a0abb38e841142208bb363713944434959abcc82
Won Rhim <wwrk22@gmail.com>
2023-09-18 17:25:48 -0400

    refactor: use sample json instead of accessing EDGAR API for test

--- spec/fipc/submission/parser_spec.rb
+++ spec/fipc/submission/parser_spec.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
+require "json"
 require "fipc/submission"
 
 RSpec.describe Fipc::Submission::Parser do
   describe ".parse" do
-    it "returns a slimmed-down version of the EDGAR submission JSON" do
-      raw_submission = Fipc::Submission::Fetcher.fetch("0000092380")
+    it "returns a processed version of the EDGAR submission JSON" do
+      # Use a saved raw submission json in place of one directly fetched from
+      # the EDGAR API. The CIK 92380 is for the company of ticker symbol LUV.
+      submission_filepath = "spec/samples/raw_submissions/CIK0000092380.json"
+      raw_submission = File.open(submission_filepath, &:read)
+
+      # Parse from JSON string to JSON.
+      raw_submission = JSON.parse(raw_submission)
+
+      # The output is a selected set of information as defined in the Parser
+      # class.
       exp_parsed_submission = {
         cik: "92380",
         name: "SOUTHWEST AIRLINES CO",
