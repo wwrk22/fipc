5d84d1b91238f928e83ac28838129b687897e1d3
Won Rhim <wwrk22@gmail.com>
2023-09-19 11:34:38 -0400

    refactor: rubocop

--- lib/fipc/company_tickers/validator.rb
+++ lib/fipc/company_tickers/validator.rb
@@ -2,13 +2,17 @@
 
 module Fipc
   class CompanyTickers
+    # Validate the contents of the SEC's company_tickers.json file. The JSON
+    # data is expected to have been parsed and given to this class as a hash.
     class Validator
       class << self
+        # Validate the top-level keys by checking that:
+        # 1. first key is "0"
+        # 2. number of items matches what the first and last keys indicate
         def validate_keys(json)
-          keys = json.keys
-          first_key, last_key = [keys.first.to_s.to_i, keys.last.to_s.to_i]
-          count = last_key - first_key + 1
-          return json.size == count
+          first_key = json.keys.first.to_s.to_i
+          last_key = json.keys.last.to_s.to_i
+          first_key.zero? && json.size == (last_key - first_key + 1)
         end
       end
     end
