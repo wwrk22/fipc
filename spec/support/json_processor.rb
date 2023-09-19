152a79190a705a2c023ac7b5c63391906749afe6
Won Rhim <wwrk22@gmail.com>
2023-09-19 12:22:51 -0400

    chore: describe test helper module

--- spec/support/json_processor.rb
+++ spec/support/json_processor.rb
@@ -2,6 +2,9 @@
 
 require "json"
 
+# Read JSON content of each file for the given directory, then pass the data
+# onto given block. Collect each block return value in an array, then return
+# the array.
 module JsonProcessor
   def process(dir)
     opened_dir = Dir.new dir
