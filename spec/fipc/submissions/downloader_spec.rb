b46eda18491ce39e6a4c828d88087d6d4300b4d3
Won Rhim <wwrk22@gmail.com>
2023-10-03 13:49:20 -0400

    test: add a test case

--- spec/fipc/submissions/downloader_spec.rb
+++ spec/fipc/submissions/downloader_spec.rb
@@ -5,10 +5,11 @@ require "fipc/submissions/downloader"
 RSpec.describe Fipc::Submissions::Downloader do
   describe ".download" do
     context "when valid user-agent and file path are given" do
+      let!(:user_agent) { "Foo Bar foobar@example.com" }
+
       context "when the HTTP request for the file is successful" do
         it "saves the file and returns a hash of info indicating success" do
-          user_agent = "Foo Bar foobar@example.com"
-          ok_response = Net::HTTPOK.new("HTTP/2", 200, "Ok")
+          ok_response = Net::HTTPOK.new("HTTP/2", 200, "OK")
           allow(ok_response).to receive(:body).and_return("fake zip file content")
           allow(Net::HTTP).to receive(:get_response)
             .with(URI(described_class::SUBMISSIONS_URL),
@@ -20,7 +21,6 @@ RSpec.describe Fipc::Submissions::Downloader do
           allow(File).to receive(:open)
             .with(described_class::NEW_SUBMISSIONS_FILE_PATH, "w")
             .and_return(1)
-
           expected = { response: ok_response,
                        file_path: "./sec_data/submissions.zip" }
 
@@ -32,7 +32,15 @@ RSpec.describe Fipc::Submissions::Downloader do
 
       context "when the HTTP request for the file fails" do
         it "does not write a file and returns a hash of info indicating failure" do
-          expected = { response: failure_response, file_path: nil }
+          not_found_response = Net::HTTPNotFound.new("HTTP/2", 404, "Not Found")
+          allow(Net::HTTP).to receive(:get_response)
+            .with(URI(described_class::SUBMISSIONS_URL),
+                  { described_class::USER_AGENT_KEY => user_agent })
+            .and_return(not_found_response)
+          expected = { response: not_found_response, file_path: nil }
+
+          result = described_class.download(user_agent: user_agent)
+
           expect(result).to eq(expected)
         end
       end
