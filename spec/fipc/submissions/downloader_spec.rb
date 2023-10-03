5b3780ea24250aaba3da78d72df0b11267e3caa1
Won Rhim <wwrk22@gmail.com>
2023-10-03 14:57:41 -0400

    refactor

--- spec/fipc/submissions/downloader_spec.rb
+++ spec/fipc/submissions/downloader_spec.rb
@@ -4,19 +4,23 @@ require "fipc/submissions/downloader"
 
 RSpec.describe Fipc::Submissions::Downloader do
   describe ".download" do
-    context "when valid user-agent and file path are given" do
-      let!(:user_agent) { "Foo Bar foobar@example.com" }
+    let!(:user_agent) { "Foo Bar foobar@example.com" }
 
-      context "when the HTTP request for the file is successful" do
+    context "when the HTTP request for the file is successful" do
+      let!(:ok_response) { Net::HTTPOK.new("HTTP/2", 200, "OK") }
+
+      before do
+        allow(ok_response).to receive(:body).and_return("fake zip file content")
+        allow(Net::HTTP).to receive(:get_response)
+          .with(URI(described_class::SUBMISSIONS_URL),
+                { described_class::USER_AGENT_KEY => user_agent })
+          .and_return(ok_response)
+      end
+
+      context "when the file is successfully written to disk" do
         let!(:test_file_size) { 1 }
 
         it "saves the file and returns a hash of info indicating success" do
-          ok_response = Net::HTTPOK.new("HTTP/2", 200, "OK")
-          allow(ok_response).to receive(:body).and_return("fake zip file content")
-          allow(Net::HTTP).to receive(:get_response)
-            .with(URI(described_class::SUBMISSIONS_URL),
-                  { described_class::USER_AGENT_KEY => user_agent })
-            .and_return(ok_response)
           allow(File).to receive(:delete)
             .with(described_class::NEW_SUBMISSIONS_FILE_PATH)
             .and_return(1)
@@ -33,32 +37,10 @@ RSpec.describe Fipc::Submissions::Downloader do
         end
       end
 
-      context "when the HTTP request for the file fails" do
-        it "does not write a file and returns a hash of info indicating failure" do
-          not_found_response = Net::HTTPNotFound.new("HTTP/2", 404, "Not Found")
-          allow(Net::HTTP).to receive(:get_response)
-            .with(URI(described_class::SUBMISSIONS_URL),
-                  { described_class::USER_AGENT_KEY => user_agent })
-            .and_return(not_found_response)
-          expected = { response: not_found_response,
-                       file_path: described_class::NEW_SUBMISSIONS_FILE_PATH }
-
-          result = described_class.download(user_agent: user_agent)
-
-          expect(result).to eq(expected)
-        end
-      end
-
       context "when a file operation raises a SystemCallError" do
         let!(:expected_error) { Errno::ENOENT.new }
 
         it "returns a hash of info indicating failure" do
-          ok_response = Net::HTTPOK.new("HTTP/2", 200, "OK")
-          allow(ok_response).to receive(:body).and_return("fake zip file content")
-          allow(Net::HTTP).to receive(:get_response)
-            .with(URI(described_class::SUBMISSIONS_URL),
-                  { described_class::USER_AGENT_KEY => user_agent })
-            .and_return(ok_response)
           allow(File).to receive(:delete)
             .with(described_class::NEW_SUBMISSIONS_FILE_PATH)
             .and_raise(expected_error)
@@ -70,6 +52,22 @@ RSpec.describe Fipc::Submissions::Downloader do
 
           expect(result).to eq(expected)
         end
+      end
+    end
+
+    context "when the HTTP request for the file fails" do
+      it "does not write a file and returns a hash of info indicating failure" do
+        not_found_response = Net::HTTPNotFound.new("HTTP/2", 404, "Not Found")
+        allow(Net::HTTP).to receive(:get_response)
+          .with(URI(described_class::SUBMISSIONS_URL),
+                { described_class::USER_AGENT_KEY => user_agent })
+          .and_return(not_found_response)
+        expected = { response: not_found_response,
+                     file_path: described_class::NEW_SUBMISSIONS_FILE_PATH }
+
+        result = described_class.download(user_agent: user_agent)
+
+        expect(result).to eq(expected)
       end
     end
   end
