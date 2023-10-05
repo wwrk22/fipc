974cc6665a46b4cddcfc02b8aa96c92b6d5701ef
Won Rhim <wwrk22@gmail.com>
2023-10-05 15:24:16 -0400

    refactor: remove unnecessary param

--- lib/fipc/cik/utility.rb
+++ lib/fipc/cik/utility.rb
@@ -7,12 +7,11 @@ module Fipc
     #   Pad a CIK with zeros to use in EDGAR API endpoints.
     class Utility
       class << self
-        def correct_format(cik, cik_str = cik.to_s)
-          # CIK cannot be greater than ten digits.
-          # Let's just return nil for now.
-          return nil if cik_str.length > 10
+        def correct_format(cik)
+          cik = cik.to_s
+          return nil if cik.length > 10
 
-          (10 - cik_str.length).times.inject(cik_str.to_s) do |padded_cik, _|
+          (10 - cik.length).times.inject(cik.to_s) do |padded_cik, _|
             "0#{padded_cik}"
           end
         end
